d67065906e6f26eb334db6bb94b53e66c06fb1dd
Meet Pandya <pandyama2@gmail.com>
2021-06-15 20:00:11 -0400

    added dynamic FormArray controls and minor scss changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,7 +13,8 @@ import { ExpenseformComponent } from './expenseform/expenseform.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/expenseform/expenseform.component.ts
+++ src/app/expenseform/expenseform.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, FormArray  } from '@angular/forms';
+
+
+/**
+ * Useful links
+ *  - https://blog.angular-university.io/angular-form-array/
+ */
+
 
 @Component({
   selector: 'app-expenseform',
@@ -7,18 +15,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpenseformComponent implements OnInit {
 
+  expenseForm: FormGroup;
+
   balance: number = 3930;  
   income: number = 5000;
   expense: string = "Rent";
   amount: number = 900;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.expenseForm = this.fb.group({
+      items2: this.fb.array([])
+    })
+   }
 
-  ngOnInit() {
+   get items(): FormArray{
+     return this.expenseForm.controls['items2'] as FormArray;
+   }
+
+   newItem(): FormGroup{
+     return this.fb.group({
+       itemName: '',
+       itemAmount: ''
+     });
+   }
+
+   removeDevice(i: number) {
+    this.items.removeAt(i);
   }
 
-  addItem(){
+   addItem(){
+     this.items.push(this.newItem());
+   }
 
+  ngOnInit() {
+    this.items.push(this.newItem());
+  }
+
+  submit(){
+    console.log(this.expenseForm.value);
   }
 
 }
